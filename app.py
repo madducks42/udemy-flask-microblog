6f553ff1f0be1647d1001f5f5477aafe0405c2fd
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2021-05-18 12:54:48 -0400

    Getting ready to push to heroku

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
+import os
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://mongo-db-user:F7BoZXmoDvdbHGWs@udemy-microblog.ld6td.mongodb.net/test")
-app.db = client.microblog
+load_dotenv()
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        app.db.entries.insert({"content": entry_content, "date": formatted_date})
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client.microblog
 
-    entries_with_date = [
-        (entry["content"], 
-        entry["date"], 
-        datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in app.db.entries.find({})
-    ] 
-    return render_template("home.html", entries=entries_with_date)
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (entry["content"], 
+            entry["date"], 
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ] 
+        return render_template("home.html", entries=entries_with_date)
+    
+    return app
